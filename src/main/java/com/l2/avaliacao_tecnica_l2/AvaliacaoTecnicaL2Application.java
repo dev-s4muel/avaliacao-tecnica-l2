c176b0bc397b3a97209fa5a5448e3973a2740004
dev-s4muel <dev.s4muelmartins@gmail.com>
2025-05-28 19:03:24 -0300

    feat: First Commit - Base Project

---
+++ src/main/java/com/l2/avaliacao_tecnica_l2/AvaliacaoTecnicaL2Application.java
@@ -0,0 +1,13 @@
+package com.l2.avaliacao_tecnica_l2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoTecnicaL2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoTecnicaL2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2/avaliacao_tecnica_l2/AvaliacaoTecnicaL2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2.avaliacao_tecnica_l2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoTecnicaL2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
